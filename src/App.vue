b8454cb7bc7b4a921f0934630906c1adbb9d6430
DDelgadoD <david.delgado82@gmail.com>
2022-10-18 12:08:27 +0200

    Adding router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
+<Navbar />
+<RouterView />
 </template>
+    
 <script setup>
+import Navbar from './components/Navbar.vue';
+console.log(import.meta.env.BASE_URL)
 </script>
-<style scoped>
-</style>
+    
