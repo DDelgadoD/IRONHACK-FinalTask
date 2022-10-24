f864e03d6f57818b4229ce38ae6521ddb0ba408e
DDelgadoD <david.delgado82@gmail.com>
2022-10-24 23:24:39 +0200

    GO

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,31 @@
-<template>
-  <nav v-if="loaded" class="navbar navbar-expand-lg navbar-light background">
+<template v-if="loaded">
+  <nav  class="navbar navbar-expand-lg navbar-light background">
     <div class="container-fluid">
       <a class="navbar-brand">
         <img class="icono" src="../assets/icon64.png" alt="Icono App" />
         <img class="logo" src="../assets/title.png" alt="ControlAPP" />
       </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse navbar-collapse justify-content-between mx-5"
-        id="navbarNav"
-      >
+      <div class="collapse navbar-collapse justify-content-between mx-5" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link
-              class="nav-link active"
-              aria-current="page"
-              :to="{ name: 'home' }"
-            >
+            <router-link class="nav-link active" aria-current="page" :to="{ name: 'home' }">
               <img class="icono" src="../assets/hogar.png" /> Home
             </router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" aria-current="page" to="/trash">
-              <img
-                v-if="discarded"
-                class="icono"
-                src="../assets/papelera-color.png"
-              />
+              <img  v-if="discarded == true" class="icono" src="../assets/papelera-color.png" />
               <img v-else class="icono" src="../assets/papelera.png" />
-              Paplelera
+              Papelera
             </router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" aria-current="page" to="/completed">
-              <img
-                v-if="completed"
-                class="icono"
-                src="../assets/objetivo-color.png"
-              />
+              <img v-if="completed == true" class="icono" src="../assets/objetivo-color.png" />
               <img v-else class="icono" src="../assets/objetivo.png" />
               Completos
             </router-link>
@@ -59,10 +37,11 @@
           </li>
         </ul>
         <div>
-          <button class="btn btn-danger" @click="onClick()">Logout</button>
+          <button class="btn btn-danger" @click="bye()">Logout</button>
         </div>
       </div>
     </div>
+
   </nav>
 </template>
 
@@ -74,29 +53,33 @@ import router from "../router";
 import { initTasks, disc, comp } from "../APIStore";
 
 const taskStore = useTaskStore();
-const discarded = ref(taskStore.discarded);
-const completed = ref(taskStore.completed);
+const discarded = ref(undefined);
+const completed = ref(undefined);
 const tasks = ref(undefined);
 const loaded = ref(false);
 const tasksF = ref({ discarded: [], completed: [], active: [] });
 
-const onClick = async () => {
+const bye = async () => {
   const ret = await desconecta();
-  alert(ret);
   router.push("/login");
 };
 
 onBeforeMount(async () => {
   tasks.value = await initTasks();
+  
   tasks.value.map((t) =>
     t.discarded
       ? tasksF.value.discarded.push(t)
       : t.completed
-      ? tasksF.value.completed.push(t)
-      : tasksF.value.active.push(t)
+        ? tasksF.value.completed.push(t)
+        : tasksF.value.active.push(t)
   );
+
   if (tasksF.value.discarded.length > 0) disc();
   if (tasksF.value.completed.length > 0) comp();
+  discarded.value = taskStore.discarded;
+  completed.value = taskStore.completed;
+
   loaded.value = true;
 });
 </script>
--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="container day-case">
     <h1>Timeline</h1>
-    <div v-if="loaded" v-for="task in tasks" class="col mb-4">
-      <div v-if="task.max_time">{{ task.max_time }}: {{ task.title }}</div>
-    </div>
+    <VueHorizontalTimeline  v-if="loaded" :items="tasks" :min-width="'250px'" 
+      :title-attr="'max_time_format'" :title-substr="30" :title-centered="true" 
+      :content-attr="'title'" :content-centered="true" :timeline-background="'#FFF'" 
+    />
   </div>
 </template>
 
 <script setup>
-// TODO: Editar el Timeline para ponerlo bonito y en orden.
+import { VueHorizontalTimeline } from "vue-horizontal-timeline";
 
 const props = defineProps({
   tasks: Object,
@@ -32,4 +33,7 @@ const props = defineProps({
   background: white;
   box-shadow: var(--day-shadow);
 }
+
+
+
 </style>
